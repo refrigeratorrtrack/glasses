c3acbda613d0e098370ec9f83e7b93abfff9cf68
refrigeratorrtrack <e.refrigeratorr@gmail.com>
2019-12-14 05:13:21 +0300

    Added and processed new data.

--- data_processing.py
+++ data_processing.py
@@ -18,7 +18,7 @@ def main():
         for file in data[key]:
             save_directory = "./data/" + key + "/" + file.split(".")[0]
             os.mkdir(save_directory)
-            x, sr = librosa.load("./data/" + key + "/" + file)
+            x, sr = librosa.load("./data/" + key + "/" + file, sr=48000)
 
             amplitude_time(x, sr, save_directory)
             spectrogram(x, sr, save_directory)
@@ -28,12 +28,49 @@ def main():
         print("It took " + str(time.time() - start_time) + " second.")
 
 
+def test():
+    x, sr = librosa.load("./data/main/main2.wav", sr=48000)
+    # print(max(x[:2666]), min(x[:2666]))
+    # for i in range(2666):
+    #     if x[i] == max(x[:2666]):
+    #         print("max: ", i)
+
+    #     if x[i] == min(x[:2666]):
+    #         print("min: ", i)
+
+    # print(max(x[2667:5332]), min(x[2666:5332]))
+    # for i in range(2667, 5332):
+    #     if x[i] == max(x[2667:5332]):
+    #         print("max: ", i)
+
+    #     if x[i] == min(x[2667:5332]):
+    #         print("min: ", i)
+    # # print(x.index(min(x[:2666])))
+    # amplitudeTime = plt.figure(figsize=(14, 5))
+    # librosa.display.waveplot(x, sr=sr)
+    # plt.show()
+    start = 0
+    end = 2666
+    for i in range(5):
+        fuckingMin = min(x[start:end])
+        fuckingMax = max(x[start:end])
+        print("max: ", fuckingMax, ", min: ", fuckingMin)
+        for j in range(start, end):
+            if x[j] == fuckingMin:
+                print("min j: ", j)
+
+            if x[j] == fuckingMax:
+                print("max j: ", j)
+
+        start += 2667
+        end += 2666
+
 def amplitude_time(x, sr, file_path):
     """ Draw amplitude-time. """
 
     amplitudeTime = plt.figure(figsize=(14, 5))
     librosa.display.waveplot(x, sr=sr)
-    amplitudeTime.savefig(file_path + "/amplitudeTime.png", dpi=1000)
+    amplitudeTime.savefig(file_path + "/amplitudeTime.png", dpi=600)
     plt.close(amplitudeTime)
 
 
@@ -69,5 +106,10 @@ def amplitude_frequency(x, sr, file_path):
     plt.close(amplitudeFrequency)
 
 
+def get_peak():
+    pass
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    test()
